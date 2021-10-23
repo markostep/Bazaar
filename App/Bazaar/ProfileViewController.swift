990f61ee909a03b0615467510a956a98499bd541
Marko Stepniczka <marko@stepniczka.com>
2021-10-23 11:58:53 -0400

    Fixed clashes with jords code

--- App/Bazaar/ProfileViewController.swift
+++ App/Bazaar/ProfileViewController.swift
@@ -8,9 +8,9 @@
 import Foundation
 import UIKit
 import Firebase
+import FirebaseAuth
 
-class ProfileViewController: UIViewController,
-UITableViewDataSource, UITableViewDelegate {
+class ProfileViewController: UIViewController {
     
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
@@ -18,25 +18,44 @@ UITableViewDataSource, UITableViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let ref = Database.database().reference()
+        let ref = Database.database().reference().child("Users")
+        ref.observeSingleEvent(of: .value) { (snapshot) in
+            guard snapshot.exists() else { return }
+            if(snapshot.exists()) {
+                let array = snapshot.children.allObjects
+                for obj in array {
+                    let snapshot:DataSnapshot = obj as! DataSnapshot
+                    if let childSnapshot = snapshot.value as? [String : AnyObject]
+                         {
+                        let name = childSnapshot["Name"] as! String
+                        let email = childSnapshot["Email"] as! String
+                        print(name)
+                        print(email)
+                        self.emailLabel.text = email
+                        self.nameLabel.text = name
+                    }
+                }
+            }
+        }
         
-        ref.child("Users/\(uid)/Name").getData(completion:  { error, snapshot in
+        /*ref.child("Users/\(Auth.auth().currentUser?.uid)/Name").getData(completion:  { error, snapshot in
           guard error == nil else {
             print(error!.localizedDescription)
             return;
           }
           let userName = snapshot.value as? String ?? "Unknown";
+            self.nameLabel.text = userName
         });
-        ref.child("Users/\(uid)/Email").getData(completion:  { error, snapshot in
+        ref.child("Users/\(Auth.auth().currentUser?.uid)/Email").getData(completion:  { error, snapshot in
           guard error == nil else {
             print(error!.localizedDescription)
             return;
           }
           let email = snapshot.value as? String ?? "Unknown";
-        });
-        nameLabel.text(userName)
-        emailLabel.text(email)
-        // Do any additional setup after loading the view.
+            self.emailLabel.text = email
+
+        });*/
+                // Do any additional setup after loading the view.
     }
     
 
