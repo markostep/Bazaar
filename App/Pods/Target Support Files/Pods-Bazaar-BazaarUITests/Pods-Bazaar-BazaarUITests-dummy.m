297df837fcc6a441da94b6ab38d0bf3381346dc7
Marko Stepniczka <marko@stepniczka.com>
2021-10-22 21:46:07 -0400

    Firebase pods installed

---
+++ App/Pods/Target Support Files/Pods-Bazaar-BazaarUITests/Pods-Bazaar-BazaarUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Bazaar_BazaarUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Bazaar_BazaarUITests
+@end
---
+++ App/Pods/Target Support Files/Pods-BazaarTests/Pods-BazaarTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BazaarTests : NSObject
+@end
+@implementation PodsDummy_Pods_BazaarTests
+@end
