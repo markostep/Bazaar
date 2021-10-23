1b01ebac64791773d575b59206add0003ecb97cd
Marko Stepniczka <marko@stepniczka.com>
2021-10-22 22:42:44 -0400

    Fixing tableviewcell scope

---
+++ App/Bazaar/SocialFeedTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  SocialFeedTableViewCell.swift
+//  Bazaar
+//
+//  Created by Marko Stepniczka on 10/22/21.
+//
+import UIKit
+
+class IncidentReportTableViewCell: UITableViewCell {
+
+    var likedState = false
+    var likedAmount = 0
+    
+
+    static let identifier = "IncidentReportTableViewCell"
+
+
+
+}
+
--- App/Bazaar/ViewController.swift
+++ App/Bazaar/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIViewControllerTransitioningDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SocialFeedTableViewCell.identifier, for: indexPath) as? SocialFeedTableViewCell
+        else { return UITableViewCell() }
+        
+        
+    }
+    
+    
+    @IBOutlet var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        self.tableView.showsVerticalScrollIndicator = false
+        
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refresh(_:)), for: .valueChanged)
+        
+        tableView.refreshControl = refreshControl
     }
 
+    @objc func refresh(_ refreshControl: UIRefreshControl) {
+        DispatchQueue.global().async {
+            /*MarkerManager.shared.refreshMarkers {
+                refreshControl.endRefreshing()
+                self.markers = MarkerManager.shared.markers
+                self.selectViewDidDisappearAction()
+                self.refreshLiked()
+                self.tableView.reloadData()
+                NotificationCenter.default.post(name: MapViewController.reloadMapNotification, object: nil)
+            }*/
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 10
+    }
+    
+    
 
 }
 
