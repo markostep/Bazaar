ce128dd5abc69cfc7967e75e72ab576d6ca25647
Marko Stepniczka <marko@stepniczka.com>
2021-10-23 13:48:08 -0400

    Added purchase page and set up profile page

---
+++ App/Bazaar/PurchaseListTableVIewCell.swift
@@ -0,0 +1,20 @@
+//
+//  PurchaseListTableVIewCell.swift
+//  Bazaar
+//
+//  Created by Marko Stepniczka on 10/23/21.
+//
+import UIKit
+
+class PurchaseListTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var storeNameLabel: UILabel!
+    @IBOutlet weak var itemNameLabel: UILabel!
+    
+    //@IBOutlet weak var circleImage: UIImageView!
+
+    static let identifier = "PurchaseListTableViewCell"
+
+
+
+}
